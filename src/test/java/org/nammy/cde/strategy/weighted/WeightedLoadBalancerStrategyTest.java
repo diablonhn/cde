dddd7e1358dd8aec6e5435b375ac226ba5129b07
diablonhn <github@diablonhn.com>
2020-01-30 02:27:30 -0800

    add test for example.txt

--- src/test/java/org/nammy/cde/strategy/weighted/WeightedLoadBalancerStrategyTest.java
+++ src/test/java/org/nammy/cde/strategy/weighted/WeightedLoadBalancerStrategyTest.java
@@ -1,16 +1,22 @@
 package org.nammy.cde.strategy.weighted;
 
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.nammy.cde.config.NodeConfig;
+import org.nammy.cde.config.NodeConfigParser;
 import org.nammy.cde.config.SystemConfig;
 import org.nammy.cde.model.QueryPlan;
 import org.nammy.cde.model.Request;
 import org.nammy.cde.model.RequestMethod;
 import org.nammy.cde.service.LoadBalancerService;
+import org.nammy.cde.service.NodeService;
 import org.nammy.cde.strategy.LoadBalancerStrategy;
 
 import static org.junit.Assert.assertEquals;
@@ -98,5 +104,60 @@ public class WeightedLoadBalancerStrategyTest {
     }
 
     assertEquals(count0, count1, 10);
+  }
+
+  @Test
+  public void test_example() throws Exception {
+    InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("example.txt");
+    List<NodeConfig> nodeConfigs = NodeConfigParser.parse(is);
+
+    LoadBalancerStrategy strategy = new ZoneRegionWeightedLoadBalancerStrategy();
+    SystemConfig systemConfig = new SystemConfig(nodeConfigs, strategy);
+
+    LoadBalancerService service = LoadBalancerService.create(systemConfig);
+
+    {
+      Request request = new Request(1, RequestMethod.GET, "region1", "zone1");
+      QueryPlan plan = service.getQueryPlan(request);
+      assertNodesEquals(plan, "node1 node2 node3 node4");
+    }
+
+    {
+      Request request = new Request(2, RequestMethod.POST, "region1", "zone1");
+      QueryPlan plan = service.getQueryPlan(request);
+      assertNodesEquals(plan, "node2");
+    }
+
+    {
+      Request request = new Request(3, RequestMethod.GET, "region2", "zone3");
+      QueryPlan plan = service.getQueryPlan(request);
+      assertNodesEquals(plan, "node4 node3 node1 node2");
+    }
+
+    {
+      Request request = new Request(4, RequestMethod.GET, "region1", "zone1");
+      QueryPlan plan = service.getQueryPlan(request);
+      assertNodesEquals(plan, "node1 node2 node3 node4");
+    }
+
+    {
+      Request request = new Request(5, RequestMethod.GET, "region2", "zone1");
+      QueryPlan plan = service.getQueryPlan(request);
+      assertNodesEquals(plan, "node3 node4 node1 node2");
+    }
+
+    {
+      Request request = new Request(6, RequestMethod.POST, "region1", "zone1");
+      QueryPlan plan = service.getQueryPlan(request);
+      assertNodesEquals(plan, "node1");
+    }
+  }
+
+  private void assertNodesEquals(QueryPlan plan, String nodeNames) {
+    String actualNodeNames = plan.getNodes().stream()
+            .map(NodeService::getName)
+            .collect(Collectors.joining(" "));
+
+    assertEquals(nodeNames, actualNodeNames);
   }
 }
